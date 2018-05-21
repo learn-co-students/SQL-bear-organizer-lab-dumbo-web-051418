68ec0978fe52cfd9462bca8d8b232d9fdb839b08
Jee Graeff <jag918@nyu.edu>
2018-05-21 14:49:49 -0400

    SQL

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Mr.Chocolate",45,"M","brown","mad","true");
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Rowdy",45,"M","brown","rowdy","true");
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Tabitha",45,"F","brown","mad","true");
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Sergeant Brown",45,"M","brown","serious","true");
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Melissa",45,"F","brown","mad","true");
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Grinch",45,"M","brown","grumpy","true");
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Wendy",45,"F","brown","friendly","true");
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES (NULL,45,"M","brown","mad","true");
